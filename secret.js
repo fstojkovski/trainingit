c7f1c02eb60f9d2ae8ac66701459199dbcb995ed
fstojkovski <33314094+fstojkovski@users.noreply.github.com>
2024-04-25 15:58:02 +0200

    Update variable name in secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 const DB_PASSWORD = 'mysecret'
-const DBPassword = 'ffefb507f32986510255f9ae8e6f7b75434eeb7703c1ea'
+const DB_PASSWORD_NEW = 'ffefb507f32986510255f9ae8e6f7b75434eeb7703c1ea'
 
-console.log(DB_PASSWORD, DBPassword)
+console.log(DB_PASSWORD, DB_PASSWORD_NEW)
